76e2efe937c61b05e750b1f0589e4266b1558333
majorsmann <majorsmann@hotmail.co.uk>
2020-06-06 19:16:46 +0100

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tolist.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# tolist
+# To do list
+
+
